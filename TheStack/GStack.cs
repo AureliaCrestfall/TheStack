0c3727e3744b452a8d70e138182fc07b6f9744de
crestfall <mas025@edu.zealand.dk>
2025-09-10 03:11:39 +0200

    rabbit

---
+++ TheStack/GStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheStack
+{
+    internal class GStack<T>
+    {
+        List<T> _gList;
+
+        public List<T> GList
+        {
+            get { return _gList; }
+            set { _gList = value; }
+        }
+
+        public void Push(T item)
+        {
+            _gList.Add(item);
+        }
+        public T Pop()
+        {
+                T pop = _gList.Last();
+                _gList.RemoveAt(_gList.Count() - 1);
+                return pop;
+        }
+
+        public T Peek()
+        {
+            return GList.Last();
+        }
+        public int Count()
+        {
+            return GList.Count();
+        }
+        public string CurrentType()
+        {  
+            return Convert.ToString(typeof(T));
+        }
+        public GStack()
+        {
+            _gList = new List<T>();
+        }
+    }
+}
--- TheStack/Program.cs
+++ TheStack/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace TheStack
+﻿using System;
+
+namespace TheStack
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool inuse = true;
+            var varStack = new GStack<int>();
+
+            
+
+
+            while (inuse == true)
+            {
+                Console.Write("do you want to get the count, push, pop or peek the stack: ");
+                string choose = Console.ReadLine().ToLower();
+                if(choose == "count")
+                {
+                    Console.WriteLine($"the current count is {varStack.Count()}");
+                }
+                else if (choose == "push")
+                {
+                    Console.WriteLine($"the current datatype is {varStack.CurrentType()}");
+                    Console.Write("what do you want to push to the stack:");
+                    try
+                    {
+                        var toPush = Console.ReadLine();
+                        varStack.Push(Convert.ToInt32(toPush));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("that does not match the datatype of the stack");
+                    }
+
+
+                }
+                else if (choose == "pop")
+                {
+                    if (varStack.Count() > 0)
+                    {
+                        Console.WriteLine($"{varStack.Pop()} has been poped from the stack");
+                    }
+                    else
+                    {
+                        Console.WriteLine("their is currently nothing in the stack to pop");
+                    }
+                }
+               else  if (choose == "peek")
+                {
+                    Console.WriteLine($"{varStack.Peek()} is currently at the top of the stack");
+                }
+                else
+                {
+                    Console.WriteLine("that was not one of the options");
+                }
+
+
+                Console.Write("do you want to continue or stop for now: ");
+                string toStopOrNot = Console.ReadLine().ToLower();
+                if (toStopOrNot == "stop")
+                {
+                    inuse = false;
+                }
+            }
+                
+
+
+
+
         }
     }
 }
